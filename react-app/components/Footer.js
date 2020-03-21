28d56730e217f7d88d46d8fb54c46c79286fd650
Nouamane Tazi <nouamane98@gmail.com>
2020-03-21 20:20:04 +0100

    Centered copyright

--- react-app/components/Footer.js
+++ react-app/components/Footer.js
@@ -5,13 +5,13 @@ const Footer = (props) => (
             <header className="text">
                 <h3>Follow us :</h3>
             </header>
-            <ul className="icons">
-                <li><a href="https://www.facebook.com/MathsMaroc2/" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
-                <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
-            </ul>
-            <ul className="copyright">
-                <li>&copy; Math&Maroc</li>
-            </ul>
+                <ul className="icons">
+                    <li><a href="https://www.facebook.com/MathsMaroc2/" className="icon alt fa-facebook"><span className="label">Facebook</span></a></li>
+                    <li><a href="#" className="icon alt fa-instagram"><span className="label">Instagram</span></a></li>
+                </ul>
+                <ul className="copyright">
+                    <li>&copy; Math&Maroc</li>
+                </ul>
         </div>
     </footer>
 )

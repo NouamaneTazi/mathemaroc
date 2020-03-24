b6b6e0ae0ea3462e3cff144a7e3899641c4693f6
Nouamane Tazi <nouamane98@gmail.com>
2020-03-24 11:12:03 +0100

    Fixed links in menu

--- react-app/components/Menu.js
+++ react-app/components/Menu.js
@@ -7,11 +7,11 @@ const Menu = ({ user, loading, onToggleMenu }) => (
            {user && <img className="user-picture" src={user.picture} alt="user picture" />}
             <ul className="links">
                 <li><Link href="/"><a><span onClick={onToggleMenu}>Home</span></a></Link></li>
-                <li><Link href="/Publications"><a><span onClick={onToggleMenu}>Publications</span></a></Link></li>
-                <li><Link href="/Olympiades"><a><span onClick={onToggleMenu}>Olympiades</span></a></Link></li>
-                <li><Link href="/Actualités"><a><span onClick={onToggleMenu}>Actualités</span></a></Link></li>
-                <li><Link href="/Stages"><a><span onClick={onToggleMenu}>Stages</span></a></Link></li>
-                {user && <li><Link href="/Profile"><a><span onClick={onToggleMenu}>Profile</span></a></Link></li>}
+                <li><Link href="/publications"><a><span onClick={onToggleMenu}>Publications</span></a></Link></li>
+                <li><Link href="/olympiades"><a><span onClick={onToggleMenu}>Olympiades</span></a></Link></li>
+                <li><Link href="/actualités"><a><span onClick={onToggleMenu}>Actualités</span></a></Link></li>
+                <li><Link href="/stages"><a><span onClick={onToggleMenu}>Stages</span></a></Link></li>
+                {user && <li><Link href="/profile"><a><span onClick={onToggleMenu}>Profile</span></a></Link></li>}
             </ul>
             <ul className="actions vertical">
                 {!user ? <li><a href="/api/login" className="button special fit">Log In</a></li>

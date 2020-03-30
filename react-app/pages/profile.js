0136e98181d241870cde1d9d7542ef8df4535ce1
Nouamane Tazi <nouamane98@gmail.com>
2020-03-30 22:07:18 +0200

    Corrections

--- react-app/pages/profile.js
+++ react-app/pages/profile.js
@@ -76,7 +76,7 @@ const Profile = () => {
                                     <h2 id="content">{user.firstname} {user.lastname}</h2>
                                     <p>Au nom de l'association Math&Maroc nous te remercions pour ton initiative, nous sommes très fiers et très content de voir qu'il y a autant de personnes prêtes à aider un grand nombre d'élèves dans le besoin. Notre but est et sera toujours d'encourager l'entraide entre marocains.
                                     <br /><br />Dans le but de suivre les tuteurs et les élèves et de s'assurer que tout se passe bien, nous te prions de <strong><u>nous faire un compte rendu rapide de chaque séance à l'aide du tableau en dessous des informations des élèves.</u></strong>
-                                        <br /><br />Si tu as rencontré un quelconque souci avec le site ou autre, nous te prions de nous contacter à l'aide de l'adresse suivante: mathemaroc.contact@gmail.com (Un screen expliquant la situation sera préférable)
+                                        <br /><br />Si tu rencontres un quelconque souci avec le site ou autre, nous te prions de nous contacter à l'aide de l'adresse suivante: mathemaroc.contact@gmail.com (Un screen expliquant la situation sera préférable)
 </p>
 
                                 </div>
@@ -144,7 +144,7 @@ const Profile = () => {
                                         <h2 id="content">{user.firstname} {user.lastname}</h2>
                                         <p>Au nom de l'association Math&Maroc nous te remercions pour ton initiative, nous sommes très fiers et très content de voir qu'il y a autant de personnes prêtes à aider un grand nombre d'élèves dans le besoin. Notre but est et sera toujours d'encourager l'entraide entre marocains.
                                     <br /><br />Dans le but de suivre les tuteurs et les élèves et de s'assurer que tout se passe bien, nous te prions de <strong><u>nous faire un compte rendu rapide de chaque séance à l'aide du tableau en dessous des informations des élèves.</u></strong>
-                                            <br /><br />Si tu as rencontré un quelconque souci avec le site ou autre, nous te prions de nous contacter à l'aide de l'adresse suivante: mathemaroc.contact@gmail.com (Un screen expliquant la situation sera préférable)
+                                            <br /><br />Si tu rencontres un quelconque souci avec le site ou autre, nous te prions de nous contacter à l'aide de l'adresse suivante: mathemaroc.contact@gmail.com (Un screen expliquant la situation sera préférable)
                                         </p>
                                         <h3>Nous allons bientôt t'attribuer un ou plusieurs élèves, merci de revenir plus tard...</h3>
 

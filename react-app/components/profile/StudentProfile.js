612d8236b77cfc33959378439f78a77b2dfd28a1
Nouamane Tazi <nouamane98@gmail.com>
2020-04-03 23:52:38 +0200

    Added student page

---
+++ react-app/components/profile/StudentProfile.js
@@ -0,0 +1,18 @@
+
+
+const Profile = ({ user }) => {
+
+    return (<>
+        <section id="one">
+            <div className="inner">
+                <header className="major">
+                    <h2>Bienvenue {user.fullname}</h2>
+                </header>
+                <p>On va bientôt t'attribuer un tuteur, donc check ta boite mail/whatsapp/facebook régulièrement pour voir si t'as reçu un message de ton tuteur.</p>
+            </div>
+        </section>
+    </>
+    )
+}
+
+export default Profile
--- react-app/pages/profile.js
+++ react-app/pages/profile.js
@@ -14,6 +14,7 @@ import Confetti from 'react-confetti'
 import useWindowSize from 'react-use/lib/useWindowSize'
 import Backdrop from '@material-ui/core/Backdrop';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import StudentProfile from '../components/profile/StudentProfile'
 
 const Profile = () => {
 
@@ -33,12 +34,14 @@ const Profile = () => {
         if (json.notYetSetUp) {
             user.needsSetup = true
         }
-        else if (json.role == "tutor" && json.groupId) {
+        else if (json.role == "tutor") {
             res = await fetch('/api/mongodb?groupId=' + json.groupId)
             json.students = await res.json()
             json.students = json.students.filter(student => student.role == "student")
             Object.assign(user, json);
             user.isSetup = true
+        } else if (json.role == "student") {
+            Object.assign(user, json);
         }
         setLoading(false)
     }
@@ -68,94 +71,95 @@ const Profile = () => {
                     <meta name="description" content="Profil" />
                 </Head>
 
-                {user && user.isSetup ? <div id="main" className="alt">
-                    {/* <Confetti width={width} height={height}/> */}
-                    <section id="one">
-                        <div className="inner">
-                            <header className="major">
-                                <h1>Profil</h1>
-                            </header>
+                {user && user.role === "student" ? <StudentProfile user={user}/>
+                    : user && user.isSetup ? <div id="main" className="alt">
+                        {/* <Confetti width={width} height={height}/> */}
+                        <section id="one">
+                            <div className="inner">
+                                <header className="major">
+                                    <h1>Profil</h1>
+                                </header>
 
-                            <div className="row 200%">
-                                <div className="12u 12u(medium)">
-                                    {/* <p><b>Nouveauté : </b> On a atteint <b style={{ fontSize: "30px" }}>100</b> séances grâce à tous vos efforts ! Toute l'équipe de Math&Maroc vous remercie pour votre contribution qui encourage la solidarité entre frères marocains et qui donne une aide précieuse à un très grand nombre d'élèves ! On compte sur vous pour continuer comme ça !</p> */}
-                                    <p><b>Nouveauté : </b> Pour ceux qui veulent avoir plus d'élèves, vous pouvez à présent choisir vous mêmes les élèves dans le catalogue à élèves en cliquant sur le bouton "Demander plus d'élèves" !</p>
-                                    <h2 id="content">{user.firstname} {user.lastname}</h2>
-                                    <p>Au nom de l'association Math&Maroc nous te remercions pour ton initiative, nous sommes très fiers et très content de voir qu'il y a autant de personnes prêtes à aider un grand nombre d'élèves dans le besoin. Notre but est et sera toujours d'encourager l'entraide entre marocains.
+                                <div className="row 200%">
+                                    <div className="12u 12u(medium)">
+                                        {/* <p><b>Nouveauté : </b> On a atteint <b style={{ fontSize: "30px" }}>100</b> séances grâce à tous vos efforts ! Toute l'équipe de Math&Maroc vous remercie pour votre contribution qui encourage la solidarité entre frères marocains et qui donne une aide précieuse à un très grand nombre d'élèves ! On compte sur vous pour continuer comme ça !</p> */}
+                                        <p><b>Nouveauté : </b> Pour ceux qui veulent avoir plus d'élèves, vous pouvez à présent choisir vous mêmes les élèves dans le catalogue à élèves en cliquant sur le bouton "Demander plus d'élèves" !</p>
+                                        <h2 id="content">{user.firstname} {user.lastname}</h2>
+                                        <p>Au nom de l'association Math&Maroc nous te remercions pour ton initiative, nous sommes très fiers et très content de voir qu'il y a autant de personnes prêtes à aider un grand nombre d'élèves dans le besoin. Notre but est et sera toujours d'encourager l'entraide entre marocains.
                                     <br /><br />Dans le but de suivre les tuteurs et les élèves et de s'assurer que tout se passe bien, nous te prions de <strong><u>nous faire un compte rendu rapide de chaque séance à l'aide du tableau en dessous des informations des élèves.</u></strong>
-                                        <br /><br />Si tu rencontres un quelconque souci avec le site ou autre, nous te prions de nous contacter à l'aide de l'adresse suivante: mathemaroc.contact@gmail.com (Un screen expliquant la situation sera préférable)
+                                            <br /><br />Si tu rencontres un quelconque souci avec le site ou autre, nous te prions de nous contacter à l'aide de l'adresse suivante: mathemaroc.contact@gmail.com (Un screen expliquant la situation sera préférable)
 </p>
 
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="12u 12u(medium)">
-                                <h2>Liste des élèves</h2>
-                                <div className="table-wrapper">
-                                    <table>
-                                        <thead>
-                                            <tr>
-                                                <th>Nom</th>
-                                                <th>Lycée</th>
-                                                <th>Ville</th>
-                                                <th>Filière</th>
-                                                <th>Matières</th>
-                                                <th>Demandes</th>
-                                                <th>Whatsapp</th>
-                                                <th>Facebook</th>
-                                                <th>Signaler</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            {user.students.map(student => (
-
-                                                <tr key={student._id} onMouseEnter={() => null}>
-                                                    <td>{student.firstname} {student.lastname}</td>
-                                                    <td>{student.lycee}</td>
-                                                    <td>{student.ville}</td>
-                                                    <td>{student.filiere}</td>
-                                                    <td>{student.matiere}</td>
-                                                    <td>{student.wishes}</td>
-                                                    <td>{student.whatsapp}</td>
-                                                    <td>{student.facebook}</td>
-                                                    <td style={{ textAlign: "center" }}> {student.reported ?
-                                                        <CustomizedTooltip title="Elève signalé" placement="left">
-                                                            <Icon style={{ fontSize: 30, verticalAlign: "text-top", color: "red", cursor: "pointer" }} onClick={() => setOpenReportDialog(student)}>warning</Icon>
-                                                        </CustomizedTooltip> :
-                                                        <CustomizedTooltip title="Signaler doublon, élève injoignable ou comportement inapproprié" placement="left">
-                                                            <Icon style={{ fontSize: 30, verticalAlign: "text-top", cursor: "pointer" }} onClick={() => setOpenReportDialog(student)}>warning</Icon>
-                                                        </CustomizedTooltip>}</td>
-                                                    <ReportStudentDialog student={openReportDialog} setOpen={setOpenReportDialog} tutor={user} />
+                                <div className="12u 12u(medium)">
+                                    <h2>Liste des élèves</h2>
+                                    <div className="table-wrapper">
+                                        <table>
+                                            <thead>
+                                                <tr>
+                                                    <th>Nom</th>
+                                                    <th>Lycée</th>
+                                                    <th>Ville</th>
+                                                    <th>Filière</th>
+                                                    <th>Matières</th>
+                                                    <th>Demandes</th>
+                                                    <th>Whatsapp</th>
+                                                    <th>Facebook</th>
+                                                    <th>Signaler</th>
                                                 </tr>
+                                            </thead>
+                                            <tbody>
+                                                {user.students.map(student => (
 
-                                            ))}
-                                        </tbody>
-                                    </table>
+                                                    <tr key={student._id} onMouseEnter={() => null}>
+                                                        <td>{student.firstname} {student.lastname}</td>
+                                                        <td>{student.lycee}</td>
+                                                        <td>{student.ville}</td>
+                                                        <td>{student.filiere}</td>
+                                                        <td>{student.matiere}</td>
+                                                        <td>{student.wishes}</td>
+                                                        <td>{student.whatsapp}</td>
+                                                        <td>{student.facebook}</td>
+                                                        <td style={{ textAlign: "center" }}> {student.reported ?
+                                                            <CustomizedTooltip title="Elève signalé" placement="left">
+                                                                <Icon style={{ fontSize: 30, verticalAlign: "text-top", color: "red", cursor: "pointer" }} onClick={() => setOpenReportDialog(student)}>warning</Icon>
+                                                            </CustomizedTooltip> :
+                                                            <CustomizedTooltip title="Signaler doublon, élève injoignable ou comportement inapproprié" placement="left">
+                                                                <Icon style={{ fontSize: 30, verticalAlign: "text-top", cursor: "pointer" }} onClick={() => setOpenReportDialog(student)}>warning</Icon>
+                                                            </CustomizedTooltip>}</td>
+                                                        <ReportStudentDialog student={openReportDialog} setOpen={setOpenReportDialog} tutor={user} />
+                                                    </tr>
+
+                                                ))}
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    {user.students.length === 0 && <p style={{ textAlign: 'center' }}>Commence par sélectionner les élèves que tu veux travailler avec en cliquant sur "Demander plus d'élèves" !</p>}
+                                    <Link href={'/catalogue'}><button className="button icon fa-plus" style={{ fontSize: "12px", marginBottom: "2em" }}>{"Demander plus d'élèves"}</button></Link>
+
                                 </div>
-                                {user.students.length === 0 && <p style={{ textAlign: 'center' }}>Commence par sélectionner les élèves que tu veux travailler avec en cliquant sur "Demander plus d'élèves" !</p>}
-                                <Link href={'/catalogue'}><button className="button icon fa-plus" style={{ fontSize: "12px", marginBottom: "2em" }}>{"Demander plus d'élèves"}</button></Link>
+
+                                <SeancesForm user={user} />
+                                <ProfileSeancesTutors />
 
                             </div>
-
-                            <SeancesForm user={user} />
-                            <ProfileSeancesTutors />
-
-                        </div>
-                    </section>
-                </div>
-                    // user not associated
-                    : user && user.needsSetup ? <AssociateUser user={user} />
-                        // Not yet connected
-                        : !loading ? <div id="main" className="alt">
-                            <section id="one">
-                                <div className="inner">
-                                    <header className="major">
-                                        <h1>Profil</h1>
-                                    </header>
-                                    <a href="/api/login" className="button special">Se connecter</a>
-                                </div>
-                            </section>
-                        </div>
-                            : null
+                        </section>
+                    </div>
+                        // user not associated
+                        : user && user.needsSetup ? <AssociateUser user={user} />
+                            // Not yet connected
+                            : !loading ? <div id="main" className="alt">
+                                <section id="one">
+                                    <div className="inner">
+                                        <header className="major">
+                                            <h1>Profil</h1>
+                                        </header>
+                                        <a href="/api/login" className="button special">Se connecter</a>
+                                    </div>
+                                </section>
+                            </div>
+                                : null
                 }
 
 

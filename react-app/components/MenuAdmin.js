bd7d6a756c029544de5e2dcc4f3e963fe16d69e1
Nouamane Tazi <nouamane98@gmail.com>
2020-04-09 23:13:23 +0200

    bugfix

--- react-app/components/MenuAdmin.js
+++ react-app/components/MenuAdmin.js
@@ -1,11 +1,15 @@
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Router from 'next/router'
+import { useEffect } from 'react'
 
 export default function ({ user }) {
     const router = useRouter()
-    if (router.pathname !== '/profile' && (!user || !user.is_admin)) Router.push('/profile')
-    if (user.is_admin) {
+    useEffect(() => {
+        if (router.pathname !== '/profile' && (!user || !user.is_admin)) Router.push('/profile')
+    }, [user])
+
+    if (user && user.is_admin) {
         return (
             <ul className="actions" style={{ display: "flex", justifyContent: 'space-evenly', width: "100%", margin: "2em 0 0 0" }}>
                 <li><Link href="/profile"><a>Profil</a></Link></li>

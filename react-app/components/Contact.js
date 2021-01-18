44e3a3f7167b6a0bd8878e3b76fee10baed44efa
Nouamane Tazi <nouamane98@gmail.com>
2021-01-18 16:47:24 +0100

    I don't get paid enough for this shit.

--- react-app/components/Contact.js
+++ react-app/components/Contact.js
@@ -1,40 +1,40 @@
 const Contact = (props) => ( 
     <section id="contact">
         <div className="inner">
-            <section> 
+            <section>  
                 <form method="post" action="#">
                     <div className="field half first">
-                        <label htmlFor="name">Name</label>
+                        <label htmlFor="name">Name</label> 
                         <input type="text" name="name" id="name" />
                     </div>
                     <div className="field half">
                         <label htmlFor="email">Email</label>
-                        <input type="text" name="email" id="email" /> 
+                        <input type="text" name="email" id="email" />  
                     </div> 
                     <div className="field">
                         <label htmlFor="message">Message</label>
                         <textarea name="message" id="message" rows="6"></textarea>
                     </div>
                     <ul className="actions">
-                        <li><input type="submit" value="Send Message" className="special" /></li>
-                        <li><input type="reset" value="Clear" /></li>
+                        <li><input type="submit" value="Send Message" className="special" /></li> 
+                        <li><input type="reset" value="Clear" /></li> 
                     </ul>
                 </form>
             </section>
             <section className="split"> 
-                <section>
+                <section> 
                     <div className="contact-method"> 
-                        <span className="icon alt fa-envelope"></span>
+                        <span className="icon alt fa-envelope"></span> 
                         <h3>Email</h3>
                         <a href="#">information@untitled.tld</a>
                     </div>
-                </section>
+                </section> 
                 <section>
                     <div className="contact-method">
                         <span className="icon alt fa-phone"></span>
                         <h3>Phone</h3>
                         <span>(000) 000-0000 x12387</span>
-                    </div>
+                    </div> 
                 </section>
                 <section>
                     <div className="contact-method"> 
@@ -45,7 +45,7 @@ const Contact = (props) => (
                         United States of America</span>
                     </div>
                 </section>
-            </section> 
+            </section>  
         </div> 
     </section>
 )

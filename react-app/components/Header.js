eaca7afe0b0614fd72a4b9258ce9248ff4478d95
Nouamane Tazi <nouamane98@gmail.com>
2021-01-19 11:34:26 +0100

    Updated build targets.

--- react-app/components/Header.js
+++ react-app/components/Header.js
@@ -1,24 +1,24 @@
-import PropTypes from 'prop-types' 
+import PropTypes from 'prop-types'  
 import Link from 'next/link'
- 
+  
 const Header = (props) => (
-    <header id="header" className="alt" style={{justifyContent:'center'}}>
+    <header id="header" className="alt" style={{justifyContent:'center'}}> 
         <Link href="/">
             <img src="/logo-white-blue.png" style={{boxSizing:'initial'}} alt="logo" className="logo" /> 
-        </Link> 
+        </Link>  
         {/* <nav> 
-            <a className="menu-link" onClick={e => {
+            <a className="menu-link" onClick={e => { 
                 e.preventDefault();
-                props.onToggleMenu(e);
+                props.onToggleMenu(e); 
                 return false; // old browsers, may not be needed 
-            }} href="javascript:;">Menu</a> 
+            }} href="javascript:;">Menu</a>  
         </nav> */}
-    </header>
-)
+    </header> 
+) 
+  
+  
+Header.propTypes = {  
+    onToggleMenu: PropTypes.func  
+} 
  
- 
-Header.propTypes = { 
-    onToggleMenu: PropTypes.func 
-}
-
 export default Header 

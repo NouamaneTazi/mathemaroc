dd7530bc618301148531b651941cd805696cb896
Nouamane Tazi <nouamane98@gmail.com>
2021-01-27 11:34:24 +0100

    SOAP is a piece of shit

--- react-app/components/BannerCovid.js
+++ react-app/components/BannerCovid.js
@@ -1,14 +1,14 @@
-const BannerLanding = (props) => (       
+const BannerLanding = (props) => (        
     <section id="banner" className="major">         
-        <div className="inner">        
-            <header className="major">        
-                <h1>Covid</h1>        
-            </header>       
-            <div className="content">        
-                <p>Remote learning with Math&Maroc during school closures</p>       
-            </div>      
+        <div className="inner">         
+            <header className="major">         
+                <h1>Covid</h1>         
+            </header>        
+            <div className="content">         
+                <p>Remote learning with Math&Maroc during school closures</p>        
+            </div>       
         </div>       
     </section>       
-)        
-        
+)         
+         
 export default BannerLanding         

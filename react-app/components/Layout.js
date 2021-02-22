8265603719e07444ffbe57b9caf7f9c6b00fb3f4
Nouamane Tazi <nouamane98@gmail.com>
2021-02-22 17:21:23 +0100

    omg what have I done?

--- react-app/components/Layout.js
+++ react-app/components/Layout.js
@@ -6,14 +6,14 @@ import Menu from './Menu'
 import Contact from './Contact'
 import Footer from './Footer'
 import Router from "next/router"
-import MenuAdmin from '../components/MenuAdmin'  
+import MenuAdmin from '../components/MenuAdmin'   
 
 class Layout extends React.Component {
     constructor(props) { 
         super(props)
         this.state = {
             isMenuVisible: false,  
-            loading: 'is-loading' 
+            loading: 'is-loading'  
         }
         this.handleToggleMenu = this.handleToggleMenu.bind(this)
     } 
@@ -24,7 +24,7 @@ class Layout extends React.Component {
         } 
         this.timeoutId = setTimeout(() => {
             this.setState({ loading: '' });
-        }, 100); 
+        }, 100);  
     } 
  
     componentWillUnmount() {
@@ -39,23 +39,23 @@ class Layout extends React.Component {
         }) 
     } 
  
-    render() {
+    render() { 
         const {user} = this.props 
         return ( 
             <div className={`body ${this.props.loading ? 'is-loading' : this.state.loading} ${this.state.isMenuVisible ? 'is-menu-visible' : ''}`}>
                 <Head> 
                     <title>Math&Maroc</title>
                     <meta name="description" content="Le site de Math&Maroc" />  
-                    <link href="/static/css/skel.css" rel="stylesheet" /> 
+                    <link href="/static/css/skel.css" rel="stylesheet" />  
                     <link href='https://cdn.jsdelivr.net/npm/react-phone-input-2@2.12.1/lib/lib.min.js' rel='stylesheet' /> 
                     <link rel="stylesheet" href="https://unpkg.com/react-phone-number-input@3.x/bundle/style.css"/>
-                    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" />
+                    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet" /> 
                     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.7.1/css/all.min.css" rel="stylesheet" />
                     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,600,600i" rel="stylesheet" />
                     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />     
                     <link
                         href="https://api.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.css" 
-                        rel="stylesheet" 
+                        rel="stylesheet"  
                     /> 
                 </Head> 
                 <style dangerouslySetInnerHTML={{ __html: stylesheet }} />
@@ -71,7 +71,7 @@ class Layout extends React.Component {
   
             </div>
         ) 
-    } 
+    }  
 }
 
 export default Layout 

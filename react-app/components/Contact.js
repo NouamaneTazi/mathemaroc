1ce756919a1c6b75607f12a9615f71e478851111
Nouamane Tazi <nouamane98@gmail.com>
2021-02-24 11:14:17 +0100

    It'd be nice if type errors caused the compiler to issue a type error

--- react-app/components/Contact.js
+++ react-app/components/Contact.js
@@ -1,23 +1,23 @@
 const Contact = (props) => (  
     <section id="contact"> 
         <div className="inner"> 
-            <section>    
+            <section>     
                 <form method="post" action="#">  
                     <div className="field half first">
                         <label htmlFor="name">Name</label>  
                         <input type="text" name="name" id="name" />  
-                    </div>   
+                    </div>    
                     <div className="field half"> 
                         <label htmlFor="email">Email</label>  
                         <input type="text" name="email" id="email" />  
                     </div>  
                     <div className="field"> 
-                        <label htmlFor="message">Message</label>  
+                        <label htmlFor="message">Message</label>   
                         <textarea name="message" id="message" rows="6"></textarea> 
-                    </div>  
-                    <ul className="actions">
+                    </div>   
+                    <ul className="actions"> 
                         <li><input type="submit" value="Send Message" className="special" /></li>   
-                        <li><input type="reset" value="Clear" /></li>  
+                        <li><input type="reset" value="Clear" /></li>   
                     </ul>
                 </form> 
             </section> 
@@ -28,7 +28,7 @@ const Contact = (props) => (
                         <h3>Email</h3>
                         <a href="#">information@untitled.tld</a>  
                     </div> 
-                </section>  
+                </section>   
                 <section>  
                     <div className="contact-method"> 
                         <span className="icon alt fa-phone"></span> 
@@ -36,18 +36,18 @@ const Contact = (props) => (
                         <span>(000) 000-0000 x12387</span>
                     </div>   
                 </section>   
-                <section>
-                    <div className="contact-method">   
+                <section> 
+                    <div className="contact-method">    
                         <span className="icon alt fa-home"></span>
-                        <h3>Address</h3>
+                        <h3>Address</h3> 
                         <span>1234 Somewhere Road #5432<br />  
                         Nashville, TN 00000<br /> 
-                        United States of America</span> 
+                        United States of America</span>  
                     </div> 
                 </section>  
-            </section>  
+            </section>   
         </div>  
     </section>
 )
-  
+   
 export default Contact

51f099892e78d8e8096ed4fd5862c9ab5762531d
Nouamane Tazi <nouamane98@gmail.com>
2021-02-28 13:09:33 +0100

    various changes

--- react-app/components/MenuAdmin.js
+++ react-app/components/MenuAdmin.js
@@ -1,18 +1,18 @@
-import Link from 'next/link' 
-import { useRouter } from 'next/router' 
+import Link from 'next/link'  
+import { useRouter } from 'next/router'  
 import Router from 'next/router' 
 import { useEffect } from 'react' 
  
-export default function ({ user }) {  
-    const router = useRouter() 
-    // useEffect(() => { 
+export default function ({ user }) {   
+    const router = useRouter()  
+    // useEffect(() => {  
     //     if (router.pathname !== '/profile' && (!user || !user.is_admin)) Router.push('/profile')   
-    // }, [user])  
-   
+    // }, [user])   
+    
     if (user && user.is_admin) { 
-        return (
+        return ( 
             <ul className="actions" style={{ display: "flex", justifyContent: 'space-evenly', width: "100%", margin: "2em 0 0 0" }}>   
-                <li><Link href="/profile"><a>Profil</a></Link></li>   
+                <li><Link href="/profile"><a>Profil</a></Link></li>    
                 <li><Link href="/seances"><a >Seances données</a></Link></li>   
                 <li><Link href="/suivi-catalogue"><a >Suivi du catalogue</a></Link></li>  
                 <li><Link href="/suivi-inscription"><a>Inscriptions tuteurs</a></Link></li>  
@@ -20,7 +20,7 @@ export default function ({ user }) {
                 <li><Link href="/reports"><a>Élèves signalés</a></Link></li> 
                 <li><Link href="/voir-groupe"><a>Voir groupe</a></Link></li> 
                 <li><Link href="/map"><a>Map</a></Link></li>  
-            </ul>   
+            </ul>    
         ) 
     } else return null  
 }  

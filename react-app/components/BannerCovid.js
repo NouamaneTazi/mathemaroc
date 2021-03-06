911c67a1ef9c3e8b9c99a45701bc102fb0e5e953
Nouamane Tazi <nouamane98@gmail.com>
2021-03-06 09:55:48 +0100

    Lock S-foils in attack position

--- react-app/components/BannerCovid.js
+++ react-app/components/BannerCovid.js
@@ -1,14 +1,14 @@
 const BannerLanding = (props) => (                          
-    <section id="banner" className="major">                         
+    <section id="banner" className="major">                          
         <div className="inner">                          
-            <header className="major">                         
-                <h1>Covid</h1>                          
-            </header>                        
-            <div className="content">                             
-                <p>Remote learning with Math&Maroc during school closures</p>                     
+            <header className="major">                          
+                <h1>Covid</h1>                           
+            </header>                         
+            <div className="content">                              
+                <p>Remote learning with Math&Maroc during school closures</p>                      
             </div>                        
-        </div>                      
+        </div>                       
     </section>                      
-)                          
-                        
-export default BannerLanding                          
+)                           
+                         
+export default BannerLanding                           

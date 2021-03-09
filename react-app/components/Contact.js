28dc40d8bddbc721a0d4e84dfd30904a105f77ff
Nouamane Tazi <nouamane98@gmail.com>
2021-03-09 21:56:57 +0100

    Trust me, I'm an engineer!... What the f*ck did just happened here?

--- react-app/components/Contact.js
+++ react-app/components/Contact.js
@@ -2,15 +2,15 @@ const Contact = (props) => (
     <section id="contact"> 
         <div className="inner"> 
             <section>     
-                <form method="post" action="#">  
+                <form method="post" action="#">   
                     <div className="field half first"> 
-                        <label htmlFor="name">Name</label>  
+                        <label htmlFor="name">Name</label>   
                         <input type="text" name="name" id="name" />   
                     </div>    
                     <div className="field half"> 
-                        <label htmlFor="email">Email</label>  
+                        <label htmlFor="email">Email</label>   
                         <input type="text" name="email" id="email" />  
-                    </div>  
+                    </div>   
                     <div className="field">  
                         <label htmlFor="message">Message</label>   
                         <textarea name="message" id="message" rows="6"></textarea> 
@@ -19,16 +19,16 @@ const Contact = (props) => (
                         <li><input type="submit" value="Send Message" className="special" /></li>    
                         <li><input type="reset" value="Clear" /></li>   
                     </ul> 
-                </form> 
+                </form>  
             </section> 
             <section className="split">  
                 <section>   
                     <div className="contact-method">  
-                        <span className="icon alt fa-envelope"></span>  
+                        <span className="icon alt fa-envelope"></span>   
                         <h3>Email</h3>
                         <a href="#">information@untitled.tld</a>   
                     </div>  
-                </section>   
+                </section>    
                 <section>  
                     <div className="contact-method"> 
                         <span className="icon alt fa-phone"></span>  
@@ -36,17 +36,17 @@ const Contact = (props) => (
                         <span>(000) 000-0000 x12387</span>
                     </div>   
                 </section>   
-                <section>  
+                <section>   
                     <div className="contact-method">     
                         <span className="icon alt fa-home"></span>
                         <h3>Address</h3> 
                         <span>1234 Somewhere Road #5432<br />  
                         Nashville, TN 00000<br /> 
-                        United States of America</span>  
+                        United States of America</span>   
                     </div> 
                 </section>  
             </section>   
-        </div>   
+        </div>    
     </section>
 )
    

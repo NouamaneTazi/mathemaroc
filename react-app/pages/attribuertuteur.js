2ed524eae5ab23b224bc560bdd86fdc425d205eb
Nouamane Tazi <nouamane98@gmail.com>
2021-03-10 18:10:20 +0100

    Made it to compile...

--- react-app/pages/attribuertuteur.js
+++ react-app/pages/attribuertuteur.js
@@ -39,7 +39,7 @@ const Reports = () => {
         const new_awaitingtutors = await res.json()       
     
         res = await fetch('/api/mongodb?getAwaitingStudents=true')  
-        const awaitingStudents = await res.json()   
+        const awaitingStudents = await res.json()    
      
         setAwaitingTutors(new_awaitingtutors) 
         setAwaitingStudents(awaitingStudents)      
@@ -52,13 +52,13 @@ const Reports = () => {
             tutor.reports[report_id].mod = { "id": user.sub, "name": user.name }  
         }    
    
-        const res = await fetch('/api/mongodb', {      
+        const res = await fetch('/api/mongodb', {       
             method: 'post',   
             body: JSON.stringify({ _id: tutor._id, data: { reports: tutor.reports } })   
         })   
         setRefresh(!refresh)     
     } 
-       
+        
     let { user, loading } = useFetchUser()   
     const [awaitingtutors, setAwaitingTutors] = useState([]) 
     const [awaitingStudents, setAwaitingStudents] = useState([])      
@@ -79,37 +79,37 @@ const Reports = () => {
                     <title>Tuteurs en attente</title>   
                     <meta name="description" content="Tuteurs en attente" />    
                 </Head>     
-                <MenuAdmin user={user}/>   
+                <MenuAdmin user={user}/>    
                 <section id="one">  
                     <div className="inner" style={{maxWidth:"75em"}}>     
                         <header className="major">      
                             {user ? <h1>Tuteurs en attente ({awaitingtutors.length})</h1>    
                                 : <h1>Vous n'êtes pas connectés</h1>}    
-                        </header>   
-      
+                        </header>    
+       
                         <div className="12u 12u(medium)">     
                             <div className="table-wrapper">    
                                 <table>  
                                     <thead>     
                                         <tr>    
                                             <th>Tuteur</th>     
-                                            <th>Statut</th>     
+                                            <th>Statut</th>      
                                             <th>Matières</th>   
                                             <th>Whatsapp</th>   
                                             <th>Mail</th>  
-                                            <th>Veut encadrer groupe ?</th>  
-                                            <th>Groupe</th>        
+                                            <th>Veut encadrer groupe ?</th>   
+                                            <th>Groupe</th>         
                                         </tr>     
                                     </thead> 
                                     <tbody>   
   
                                         {awaitingtutors.map(tutor => (  
-                                            <tr key={`${tutor._id}`}> 
+                                            <tr key={`${tutor._id}`}>  
                                                 <td>{tutor.firstname} {tutor.lastname}</td>      
                                                 <td>{tutor.statut}</td> 
                                                 <td>{tutor.matieres}</td>   
                                                 <td>{tutor.whatsapp}</td>     
-                                                <td>{tutor.mail}</td>   
+                                                <td>{tutor.mail}</td>    
                                                 <td>{tutor.encadrer_groupe ? "Oui" : "Non"}</td>    
                                                 <td style={{verticalAlign:"middle"}}><InputGroupId tutor={tutor} /></td>    
                                             </tr>     
@@ -117,7 +117,7 @@ const Reports = () => {
                                         )}   
                                     </tbody>  
                                 </table>      
-                            </div>          
+                            </div>           
                         </div>      
                     </div>  
       

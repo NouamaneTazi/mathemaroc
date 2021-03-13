c2f3860dd51016b96a54050cdba35a74a4d3cfb8
Nouamane Tazi <nouamane98@gmail.com>
2021-03-13 13:46:33 +0100

    more stuff

--- react-app/components/ProfileSeancesTutors.js
+++ react-app/components/ProfileSeancesTutors.js
@@ -17,7 +17,7 @@ const ProfileSeancesTutors = ({setConfettis}) => {
         if (nb_seances >= 500) setConfettis(true)
         return nb_seances  
     }
-    const getNumberStudents = async () => {
+    const getNumberStudents = async () => { 
         const res = await fetch('/api/mongodb?count=students')
         const counts = await res.json()
         setCounts(counts) 
@@ -33,14 +33,14 @@ const ProfileSeancesTutors = ({setConfettis}) => {
             <h2>Tu n'es pas seul !</h2>  
             <p>Voici toute une communauté qui travaille aussi pour aider une génération à étinceler. S’entre aider c’est réussir ensemble.<br /> Ensemble on va plus loin, on crée un vrai impact.</p>
             <div className='row'>
-                <div className="6u 12u$(small)">   
+                <div className="6u 12u$(small)">    
                     <h3>Compteur de séances données :</h3>
                     <div className="box" style={{ textAlign: "center" }}> 
                         <h1>{getNumberSeances(tutors)}</h1>
                     </div>
                 </div>
                 <div className="6u 12u$(small)">
-                    <h3>Compteur d'élèves pris en charge :</h3>  
+                    <h3>Compteur d'élèves pris en charge :</h3>   
                     <div className="box" style={{ textAlign: "center" }}>
                         <h1>{counts.supportedStudents} / {counts.students}</h1> 
                     </div> 
@@ -56,15 +56,15 @@ const ProfileSeancesTutors = ({setConfettis}) => {
                             <th>Durée</th>
                             <th>Chapitres traités</th>
                         </tr>
-                    </thead>
-                    <tbody> 
+                    </thead> 
+                    <tbody>  
                         {tutors.sort((b,a)=> moment(a.last_updated) - moment(b.last_updated)).slice(0, maxRows).map(tutor => {  
                             return (  
                                 <Fragment key={`${tutor._id}`}>
                                     {tutor.seances && tutor.seances.map((seance, index) => ( 
                                         <tr key={`${tutor._id}~${index}`}>  
                                             {index == 0 && <td rowSpan={tutor.seances.length} style={{ verticalAlign: "middle" }}><b>{tutor.firstname} {tutor.lastname}</b></td>}
-                                            {index == 0 && <td rowSpan={tutor.seances.length} style={{ verticalAlign: "middle" }}><b>{moment(tutor.last_updated).format('DD/MM/YYYY HH:mm:ss')}</b></td>}
+                                            {index == 0 && <td rowSpan={tutor.seances.length} style={{ verticalAlign: "middle" }}><b>{moment(tutor.last_updated).format('DD/MM/YYYY HH:mm:ss')}</b></td>} 
                                             <td>{moment(seance.date).format('DD/MM/YYYY')}</td>
                                             <td>{seance.duree}</td>  
                                             <td>{seance.chapitres}</td>
@@ -76,11 +76,11 @@ const ProfileSeancesTutors = ({setConfettis}) => {
                         })}
                     </tbody>
                 </table>
-                <p style={{ textAlign: "center", cursor: "pointer" }} onClick={() => setMaxRows(maxRows + 10)}><b>Voir plus...</b></p> 
+                <p style={{ textAlign: "center", cursor: "pointer" }} onClick={() => setMaxRows(maxRows + 10)}><b>Voir plus...</b></p>  
             </div> 
    
         </div>  
-    )
+    ) 
 }
  
 export default ProfileSeancesTutors

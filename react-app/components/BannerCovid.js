50e50cfc02c54f68bb7dd406f001ed19426eddef
Nouamane Tazi <nouamane98@gmail.com>
2021-03-27 01:54:03 +0100

    Never gonna make you cry

--- react-app/components/BannerCovid.js
+++ react-app/components/BannerCovid.js
@@ -1,14 +1,14 @@
 const BannerLanding = (props) => (                                  
-    <section id="banner" className="major">                                   
-        <div className="inner">                                 
+    <section id="banner" className="major">                                    
+        <div className="inner">                                  
             <header className="major">                                    
                 <h1>Covid</h1>                                    
             </header>                                 
             <div className="content">                                      
-                <p>Remote learning with Math&Maroc during school closures</p>                               
-            </div>                              
-        </div>                               
-    </section>                                
+                <p>Remote learning with Math&Maroc during school closures</p>                                
+            </div>                               
+        </div>                                
+    </section>                                 
 )                                    
                                   
-export default BannerLanding                                   
+export default BannerLanding                                    
--- react-app/components/MenuAdmin.js
+++ react-app/components/MenuAdmin.js
@@ -1,20 +1,20 @@
-import Link from 'next/link'  
-import { useRouter } from 'next/router'  
-import Router from 'next/router' 
+import Link from 'next/link'   
+import { useRouter } from 'next/router'   
+import Router from 'next/router'  
 import { useEffect } from 'react'   
-  
+   
 export default function ({ user }) {    
-    const router = useRouter()   
+    const router = useRouter()    
     // useEffect(() => {    
     //     if (router.pathname !== '/profile' && (!user || !user.is_admin)) Router.push('/profile')    
     // }, [user])    
       
     if (user && user.is_admin) {  
-        return (  
+        return (   
             <ul className="actions" style={{ display: "flex", justifyContent: 'space-evenly', width: "100%", margin: "2em 0 0 0" }}>    
                 <li><Link href="/profile"><a>Profil</a></Link></li>    
-                <li><Link href="/seances"><a >Seances données</a></Link></li>   
-                <li><Link href="/suivi-catalogue"><a >Suivi du catalogue</a></Link></li>   
+                <li><Link href="/seances"><a >Seances données</a></Link></li>    
+                <li><Link href="/suivi-catalogue"><a >Suivi du catalogue</a></Link></li>    
                 <li><Link href="/suivi-inscription"><a>Inscriptions tuteurs</a></Link></li>    
                 <li><Link href="/suivi-inscription2"><a>Inscriptions élèves</a></Link></li>    
                 <li><Link href="/reports"><a>Élèves signalés</a></Link></li> 
@@ -23,4 +23,4 @@ export default function ({ user }) {
             </ul>     
         ) 
     } else return null   
-}  
+}   

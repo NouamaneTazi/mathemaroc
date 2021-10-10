5154fa1847efd92579e2a8ab11dde0f7e15f1bb8
Nouamane Tazi <nouamane98@gmail.com>
2021-10-11 00:25:22 +0200

    fix socials links

--- src/config/site.js
+++ src/config/site.js
@@ -5,12 +5,10 @@ const siteConfig = {
   twitterUsername: "",
   email: "mathsmcontact@gmail.com",
   socials: {
-    Discord: "https://discord.gg/kx3qp5S",
-    GitHub: "https://github.com/surabayajs",
-    Twitch: "https://twitch.tv/surabayajs",
-    Twitter: "https://twitter.com/surabaya_js",
     Youtube: "https://www.youtube.com/channel/UCHkB77QEg1w-1E9bmsGKDYg",
     Facebook: "https://www.facebook.com/MathsMaroc2",
+    Instagram:
+      "https://www.instagram.com/mathmaroc/?fbclid=IwAR343J4433fy9rVgCFLbDK_MnwCn7lIDnrd_a6ACIp9vkrt1F8DSN5ZJSIU",
   },
   themeColor: "#4299E1",
 };

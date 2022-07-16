59023b8ca525f475242ac0f3e0bcfeb710a74765
Nouamane Tazi <nouamane98@gmail.com>
2022-07-16 22:44:07 +0200

    fix nested pages in student

--- src/components/navbar/navbar-desktop.tsx
+++ src/components/navbar/navbar-desktop.tsx
@@ -7,7 +7,18 @@ import { LocaleButton } from "@/components/locale-button";
 // import { useEmail } from "@/hooks/app";
 import routes from "@/routes";
 
-import { Button, Container, Divider, HStack, Spacer, Stack } from "@chakra-ui/react";
+import {
+  Button,
+  Container,
+  Divider,
+  HStack,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spacer,
+  Stack,
+} from "@chakra-ui/react";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
 
@@ -20,14 +31,32 @@ export const NavbarDesktop: React.FC = () => {
   return (
     <Container as={Stack} d={{ base: "none", md: "flex" }} maxW="6xl" py={4} spacing={4}>
       <HStack>
-        {Object.entries(routes(locale)).map(([href, { name, ext = false }]) => (
-          <NextLink key={name} href={href} passHref>
-            <Button as="a" {...(ext ? { target: "_blank" } : {})} variant="ghost">
-              {name}
-            </Button>
-          </NextLink>
-        ))}
-
+        {Object.entries(routes(locale)).map(([href, { name, ext = false, submenu }]) =>
+          submenu ? (
+            <Menu>
+              <MenuButton as={Button} variant="ghost">
+                {name}
+              </MenuButton>
+              <MenuList>
+                {submenu.map(({ sub_name, sub_href }) => (
+                  <MenuItem key={sub_name}>
+                    <NextLink href={sub_href} passHref>
+                      <Button as="a" {...(ext ? { target: "_blank" } : {})} variant="ghost">
+                        {sub_name}
+                      </Button>
+                    </NextLink>
+                  </MenuItem>
+                ))}
+              </MenuList>
+            </Menu>
+          ) : (
+            <NextLink key={name} href={href} passHref>
+              <Button as="a" {...(ext ? { target: "_blank" } : {})} variant="ghost">
+                {name}
+              </Button>
+            </NextLink>
+          ),
+        )}
         <Spacer />
 
         {/* <EmailTooltip>
--- src/routes.js
+++ src/routes.js
@@ -5,8 +5,13 @@ const routes = {
     "/about": { name: "Who are we?" },
     "/contact": { name: "Contact" },
     "/conferences": { name: "Conferences" },
-    "/student": { name: "For students" },
-    "/student/orientation": { name: "Orientation" },
+    "/student": {
+      name: "For students",
+      submenu: [
+        { sub_name: "Student's guide", sub_href: "/student/guide" },
+        { sub_name: "Orientation", sub_href: "/student/orientation" },
+      ],
+    },
     "/journal": { name: "Journal" },
     "/olympiads": { name: "Olympiads" },
   },
@@ -21,8 +26,13 @@ const routes = {
     "/about": { name: "من نحن؟" },
     "/contact": { name: "Contact" },
     "/conferences": { name: "Conferences" },
-    "/student": { name: "For students" },
-    "/student/orientation": { name: "Orientation" },
+    "/student": {
+      name: "For students",
+      submenu: [
+        { sub_name: "Student's guide", sub_href: "/student/guide" },
+        { sub_name: "Orientation", sub_href: "/student/orientation" },
+      ],
+    },
     "/journal": { name: "Journal" },
     "/olympiads": { name: "Olympiads" },
   },
@@ -30,6 +40,6 @@ const routes = {
 
 /**
  * @param {string} locale
- * @returns {Record<string,{name:string;ext?:boolean;}>}
+ * @returns {Record<string,{name:string;ext?:boolean;submenu?:{sub_name:string;sub_href:string}[]}>}
  */
 module.exports = (locale) => routes[locale];

0a484f6bfb84f4030d6cd683d38c1edf3533c741
Nouamane Tazi <nouamane98@gmail.com>
2022-08-06 00:17:18 +0200

    add picture to About page

--- src/modules/about/aboutEn.tsx
+++ src/modules/about/aboutEn.tsx
@@ -1,6 +1,9 @@
 const aboutEn = `
 # About
 Math&Maroc is an association founded by young Moroccan polytechnicians whose objective is to promote mathematics in Morocco. Our main action consists in preparing brilliant Moroccan students to represent our country in international mathematics competitions. For this, we help the Ministry of Education in this heavy task by sending supervisors during preparation courses. We also want to combine this punctual preparation with a long term follow-up through e-learning. This learning will take place on a platform on which students will have to follow regular courses and send monthly exercises. We also publish a monthly magazine specialized in mathematics olympiads. This new experience in Morocco is based on the dedication of several Moroccans from French Grandes Ecoles or having participated in the International Olympiads.
+
+&nbsp;
+![](https://scontent-cdg2-1.xx.fbcdn.net/v/t1.6435-9/67441437_2312798345483081_6144661117403660288_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=e3f864&_nc_ohc=-tuHoR4IhyQAX9n7AKU&tn=PLoQ3sODeEvZL2rM&_nc_ht=scontent-cdg2-1.xx&oh=00_AT9M6swUGjbWpGyU_onKp1WqWCMTwlQE2eVuK8w3nPSEmA&oe=6312172E)
 `.trim();
 
 export default aboutEn;

852384a076b3e296072b58556b6239a99bf1532f
Nouamane Tazi <nouamane98@gmail.com>
2022-08-14 15:49:39 +0100

    add competition initial module and translations

---
+++ src/modules/competition/competitonFr.tsx
@@ -0,0 +1,9 @@
+const competition = `
+# Competition
+Math&Maroc est une association fondée par de jeunes polytechniciens marocains dont l’objectif est de promouvoir les mathématiques au Maroc. Notre action principale consiste à préparer de brillants élèves marocains pour représenter notre pays lors de compétitions internationales de mathématiques. Pour cela, nous aidons le Ministère de l'Éducation Nationale dans cette lourde tache en envoyant des encadrants pendant des stages de préparation. Nous voulons également allier cette préparation ponctuelle à un suivi sur le long terme par de l’apprentissage en ligne. Cet apprentissage s’effectuera sur une plateforme sur laquelle les élèves devront régulièrement suivre des cours et envoyer mensuellement des exercices. Nous publions également un mensuel spécialisé dans les olympiades de mathématiques. Cette expérience nouvelle au Maroc repose sur le dévouement de plusieurs Marocains des grandes écoles françaises ou ayant participé aux Olympiades internationales.
+
+&nbsp;
+![](https://scontent-cdg2-1.xx.fbcdn.net/v/t1.6435-9/67441437_2312798345483081_6144661117403660288_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=e3f864&_nc_ohc=-tuHoR4IhyQAX9n7AKU&tn=PLoQ3sODeEvZL2rM&_nc_ht=scontent-cdg2-1.xx&oh=00_AT9M6swUGjbWpGyU_onKp1WqWCMTwlQE2eVuK8w3nPSEmA&oe=6312172E)
+`.trim();
+
+export default competition;
--- src/pages/competition.tsx
+++ src/pages/competition.tsx
@@ -1,20 +1,20 @@
 import * as React from "react";
 
-import i18n from "@/i18n";
-import { contentRenderer } from "@/utils/renderers";
+import { GetStaticPropsContext, NextPage } from "next";
 
 import { Container } from "@chakra-ui/react";
-import { GetStaticPropsContext, NextPage } from "next";
 import { NextSeo } from "next-seo";
 import ReactMarkdown from "react-markdown";
+import { contentRenderer } from "@/utils/renderers";
+import i18n from "@/i18n";
 
-// const aboutUrl =
-//   "https://raw.githubusercontent.com/NouamaneTazi/mathemaroc/master/markdowns/about.mdx";
+// const competitionUrl =
+//   "https://raw.githubusercontent.com/NouamaneTazi/mathemaroc/master/markdowns/competition.mdx";
 
 export async function getStaticProps(args: GetStaticPropsContext) {
   const locale = args.locale as string;
-  const content = i18n.about.page[locale] as string;
-  // const content = await fetch(aboutUrl).then((res) => res.text());
+  const content = i18n.competition.page[locale] as string;
+  // const content = await fetch(competitionUrl).then((res) => res.text());
 
   return {
     props: {
@@ -24,16 +24,16 @@ export async function getStaticProps(args: GetStaticPropsContext) {
   };
 }
 
-interface aboutPageProps {
+interface competitionPageProps {
   content: string;
   locale: string;
 }
 
-const CompetitionPage: NextPage<aboutPageProps> = (props) => {
+const CompetitionPage: NextPage<competitionPageProps> = (props) => {
   const { locale, content } = props;
   return (
     <>
-      <NextSeo title={i18n.about.title[locale] as string} />
+      <NextSeo title={i18n.competition.title[locale] as string} />
       <Container as="section" maxW="6xl" pt={[4, 8]} px={[4, 8]}>
         <ReactMarkdown children={content} components={contentRenderer} />
       </Container>

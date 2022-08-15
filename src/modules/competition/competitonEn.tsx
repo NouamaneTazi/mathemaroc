1aac9a3bb5dc7f776bb2d03a5cbc463adec819c9
Abir <abirharrasse@gmail.com>
2022-08-15 18:36:32 +0100

    2023 Contest

--- src/modules/competition/competitonEn.tsx
+++ src/modules/competition/competitonEn.tsx
@@ -1,7 +1,13 @@
 const competitionEn = `
 # Competition
-Math&Maroc is an association founded by young Moroccan polytechnicians whose objective is to promote mathematics in Morocco. Our main action consists in preparing brilliant Moroccan students to represent our country in international mathematics competitions. For this, we help the Ministry of Education in this heavy task by sending supervisors during preparation courses. We also want to combine this punctual preparation with a long term follow-up through e-learning. This learning will take place on a platform on which students will have to follow regular courses and send monthly exercises. We also publish a monthly magazine specialized in mathematics olympiads. This new experience in Morocco is based on the dedication of several Moroccans from French Grandes Ecoles or having participated in the International Olympiads.
-
+### INTRODUCTION:
+ The **MATH&MAROC Contest** for first year university students is a math contest planned for students completing their first year students in university or CPGE. The first edition of this competition will be taking place online ***Saturday 6th of May 2023***. 
+### PARTICIPATION:
+ A registration platform will be uploaded in due time. All participants are required to be in possession of a *webcam* and a *stable internet connection*. It is expected that the participants ***strictly adhere*** to the rules.
+ ### CONTEST PROBLEMS
+ The contest will be held for *4 hours*. There will be *5 problems* from the fields of ***mathematics*** included in *regular curriculums*. More details will be available in due time. The problems will be available in ***English*** and ***French***. The participants are free to compose in whatever language they choose of the aforementioned two.
+ ###  RULES AND REGULATION: 
+  The rules of the competition will be decided at a later date. More details will be available, please consult this page **regularly**.
 &nbsp;
 ![](https://scontent-cdg2-1.xx.fbcdn.net/v/t1.6435-9/67441437_2312798345483081_6144661117403660288_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=e3f864&_nc_ohc=-tuHoR4IhyQAX9n7AKU&tn=PLoQ3sODeEvZL2rM&_nc_ht=scontent-cdg2-1.xx&oh=00_AT9M6swUGjbWpGyU_onKp1WqWCMTwlQE2eVuK8w3nPSEmA&oe=6312172E)
 `.trim();

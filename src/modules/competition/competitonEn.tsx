1bc7606c054c13eaba830c3fb53a1f960505c5e3
Nouamane Tazi <nouamane98@gmail.com>
2023-03-01 13:56:22 +0100

    add competition details

--- src/modules/competition/competitonEn.tsx
+++ src/modules/competition/competitonEn.tsx
@@ -1,19 +1,45 @@
 const competitionEn = `
-# Math&Maroc Competition for First Year University Students
-![](https://i.ibb.co/MsdKFgm/LOGO-removebg-preview.png)
-### Saturday 6th May 2023
+# Math&Maroc Competition
+![](https://i.imgur.com/zuVAs3U.jpg)
 
-## Introduction:
-The **MATH&MAROC Contest** for first year university students is a math contest planned for students completing their first year students in university or CPGE. The first edition of this competition will be taking place online ***Saturday 6th of May 2023***. 
+## News:
+Les inscriptions sont ouvertes ! Voir les modalités ci-dessous.
 
-## Participation:
-A registration platform will be uploaded in due time. All participants are required to be in possession of a *webcam* and a *stable internet connection*. It is expected that the participants ***strictly adhere*** to the rules.
 
-## Contest Problems:
-The contest will be held for *4 hours*. There will be *5 problems* from the fields of ***mathematics*** included in *regular curriculums*. More details will be available in due time. The problems will be available in ***English*** and ***French***. The participants are free to compose in whatever language they choose of the aforementioned two.
+## Qu'est-ce que Math&Maroc Competition?
+**Math&Maroc Competition (MMC)** est une compétition de mathématiques organisée par l'association Math&Maroc, en collaboration avec le département Al Khawarizmi (Université Mohammed VI Polytechnique UM6P) et notre sponsor officiel Adria B&T.
+Elle vise à développer l'esprit mathématique, la créativité et l'excellence des étudiants marocains. Ce sera aussi l'occasion de faire connaissance avec les membres de l'association et d'autres étudiants passionnés par les mathématiques, de participer à de nombreuses activités et conférences, et de vivre une expérience unique au sein de l'UM6P. 
 
-## Rules and Regulation: 
-The rules of the competition will be decided at a later date. More details will be available, please consult this page **regularly**.
-`.trim();
+## Pour qui?
+Etudiants en première année d'études supérieures dans les universités, classes préparatoires (CPGE) et écoles d'ingénieurs post-bac.
+
+## Quand?
+Du samedi 29 avril soir au lundi 1er mai matin.
+
+## Où?
+Dans les locaux de l'UM6P à Benguérir. Le logement et la restauration des participants seront pris en charge sur place (voir règlement de le compétition).
+
+## Quand s'inscrire?
+Du 01/03/2023 au 01/04/2023 (23:59 heure du Maroc).
+
+## Comment s'inscrire?
+* Lire attentivement le règlement de la compétition: Reglement de la competition MMC.pdf
+* Remplir le formulaire d'inscription: [**(lien du formulaire)**](https://forms.gle/oPCRhQuRXfoY282X7)
+* N.B.: Le formulaire demande de télécharger votre certificat de scolarité 2022-2023, votre CINE, votre bulletin de notes du 1er trimestre/semestre 2022-2023 (si disponible), ainsi que le règlement signé.
+* Merci de ne pas remplir le formulaire avant d'être sûr que vous pouvez être présent sur place.
+* Le nombre de places prévues est de 150 personnes. S'il y a plus de demande, le comité d'organisation procédera à une sélection sur la base des réponses au formulaire (question de motivation, bulletin, représentativité de tous les profils). Par conséquent, vous ne serez considéré définitivement inscrit qu'après avoir reçu un mail de confirmation après la clôture de la phase d'inscription (après le 01/04/2023). Vous recevrez dans tous les cas un mail donnant suite à votre demande, merci de ne pas nous relancer sur ce sujet.
+* Après le 01/04/2023 et jusqu'à la veille de la compétition, il sera possible de s'inscrire en liste d'attente. Les candidats dans ce cas seront contactés individuellement en cas de place disponible.
+* Pour toute question, difficulté de déplacement, besoin d'aide financière ou demande spéciale de dérogation au règlement, merci de nous contacter par mail math.maroc.competition@gmail.com. Aucune demande spéciale ou dérogation au règlement ne pourra être accordée si elle ne passe pas par ce canal.
+
+
+## Programme indicatif:
+![](https://imgur.com/YGuegnk.png)
+
+
+
+![](https://i.imgur.com/miI83cm.jpg)
+<img src="https://i.imgur.com/miI83cm.jpg" width="100" height="100">
+
+`;
 
 export default competitionEn;

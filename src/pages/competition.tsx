ba770176d97308d82da4884329370018e15a3983
Nouamane Tazi <nouamane98@gmail.com>
2023-03-01 17:38:49 +0100

    quick fixes

--- src/pages/competition.tsx
+++ src/pages/competition.tsx
@@ -1,32 +1,32 @@
 import * as React from "react";
 
-import i18n from "@/i18n";
-import { contentRenderer } from "@/utils/renderers";
-
 import { Box, Container, Image } from "@chakra-ui/react";
 import { GetStaticPropsContext, NextPage } from "next";
+
 import { NextSeo } from "next-seo";
 import ReactMarkdown from "react-markdown";
+import { contentRenderer } from "@/utils/renderers";
+import i18n from "@/i18n";
 
 // const competitionUrl =
 //   "https://raw.githubusercontent.com/NouamaneTazi/mathemaroc/master/markdowns/competition.mdx";
 
 contentRenderer.img = (props) => {
   const { src } = props;
-  if (src === "https://imgur.com/YGuegnk.png") {
+  if (src === "https://i.imgur.com/HCnUEiO.png" || src === "https://imgur.com/YGuegnk.png") {
     return (
       <Box>
-        <Image {...props} height="50px" width="auto" margin="auto" />
+        <Image {...props} maxWidth="90%" margin="auto" />
       </Box>
     );
   } else if (src === "https://i.imgur.com/miI83cm.jpg") {
     return (
       <Box>
-        <Image {...props} height="700px" width="auto" margin="20px auto auto auto" />
+        <Image {...props} maxHeight="50em" margin="3em auto auto auto" />
       </Box>
     );
   } else {
-    return <Image {...props} />;
+    return <Image {...props} maxWidth="50%" margin="auto" />;
   }
 };
 
